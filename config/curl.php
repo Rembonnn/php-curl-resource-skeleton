4fcccbf95c19cee1403f1fbf11c15b79ebf8fb5f
dayCod <alayarahman07@gmail.com>
2023-11-06 11:52:03 +0700

    add skeleton

---
+++ config/curl.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'data' => [
+        'order' => [
+            'parameter' => 'date_created', // date_created|date_modified
+            'type' => 'DESC', // ASC|DESC
+        ],
+        'pagination' => [
+            'perpage' => 10,
+        ],
+    ],
+];
